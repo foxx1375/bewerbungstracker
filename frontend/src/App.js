1c513c066875d418df17de60eb86a6e48ccd05da
Frank Buetefuer <fbuetefuer@gmail.com>
2025-01-11 16:18:03 +0100

    Updated UI and fixed layout issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ApplicationList from './components/ApplicationList';
 import ApplicationForm from './components/ApplicationForm';
+import './App.css';  // Stelle sicher, dass die CSS-Datei importiert wird
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container"> {/* Um den gesamten Inhalt zentrieren */}
       <h1>Bewerbungstracker</h1>
       <ApplicationForm onSuccess={() => console.log('Form submitted!')} />
       <ApplicationList />
